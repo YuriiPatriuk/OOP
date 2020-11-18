9d72c7d678832a2c9d3c9f1cecbf458961623848
yurii <juragagarin223@gmail.com>
2020-11-18 16:55:05 +0200

    finish set

---
+++ lesson 1/2 kurs lesson 1/Pract.cpp
@@ -0,0 +1,222 @@
+﻿#include<iostream>
+#include<string>
+#include<iomanip>
+using namespace std;
+
+//Реализуйте класс “Студент”.Необходимо хранить в переменных - членах класса : ФИО, дату рождения, 
+//контактный телефон, город, страну, название учебного заведения, город и страну(где находится учебное заведение), 
+//номер группы.Реализуйте функции - члены класса для ввода данных, вывода данных, реализуйте аксессоры для доступа 
+//к отдельным переменным - членам.
+
+class Student 
+{
+public:
+	class Date
+	{
+		static const size_t CURR_YEAR = 2020;
+		bool isLeap(size_t year) {
+			return year % 4 == 0 && year % 100 != 0 && year % 400 == 0;
+		}
+	private:
+		size_t day, month, year;
+	public:
+		void setMonth(const size_t & month_)
+		{
+			if (month_>=1&&month_<=12)
+			{
+				month = month_;
+			}
+		}
+		void setYear(const size_t& year_)
+		{
+			if (year_>=CURR_YEAR-18)
+			{
+				year = year_;
+			}
+		}
+		void setDay(size_t& day_)
+		{
+			size_t dayInMonth[]{31,28,31,30,31,30,31,31,30,31,30,31};
+			if (day_ >= 1 && day_ <= dayInMonth[month - 1])
+			{
+				day = day_;
+			}
+			else if (month == 2 && day_ == 29 && isLeap(year) )
+			{
+				day = day_;
+			}
+		}
+		void input()
+		{
+			do {
+				cout << "Enter day: ";
+				cin >> day;
+				cout << "Enter month: ";
+				cin >> month;
+				cout << "Enter year: ";
+				cin >> year;
+				if (day > 0 && day <= 28 && month > 0 && month <= 12 && year>CURR_YEAR-18)
+				{
+					cout << "Enter date successfull!" << endl;
+					break;
+				}
+				else if (month == 2 && day == 29 && isLeap(year))
+				{
+					cout << "Enter date successfull!" << endl;
+					break;
+				}
+				if (day<0 || day>28) cout << "DAY ERROR ";
+				if (month <= 0 || month > 12) cout << "MONTH ERROR ";
+				if (year < 0 || year > CURR_YEAR-18) cout << "YEAR ERROR ";
+			} while (true);
+		}
+		void print()
+		{
+			cout << day << "." << month << "." << year << endl;
+		}
+	};
+	void input() {
+		cout << "Enter name: ";
+		cin >> name;
+		cout << "Enter surname: ";
+		cin >> surname;
+		cout << "Enter fathername: ";
+		cin >> fathername;
+		cout << "Enter date of birthday: ";
+		date.input();
+		cout << "Enter number: ";
+		cin >> number;
+	}
+	void setName(const string& newName)
+	{
+		if (!newName.empty())
+		{
+			name = newName;
+		}
+		else
+		{
+			cout << "Name Error!" << endl;
+		}
+	}
+	void setSurname(const string &newSurname)
+	{
+		if (!newSurname.empty())
+		{
+			surname = newSurname;
+		}
+		else
+		{
+			cout << "Surname Error!" << endl;
+		}
+	}
+	void setFathername(const string& newFathername)
+	{
+		if (!newFathername.empty())
+		{
+			fathername = newFathername;
+		}
+		else
+		{
+			cout << "Fathername Error!" << endl;
+		}
+	}
+	void setNumber(const string& newNumber)
+	{
+		number = newNumber;
+	}
+	void setCity(const string& newCity)
+	{
+		if (!newCity.empty())
+		{
+			city = newCity;
+		}
+		else
+		{
+			cout << "City Error!" << endl;
+		}
+	}
+	void setCountry(const string& newCountry)
+	{
+		if (!newCountry.empty())
+		{
+			country = newCountry;
+		}
+		else
+		{
+			cout << "Country Error!" << endl;
+		}
+	}
+	void setUniver(const string& newUniver)
+	{
+		if (!newUniver.empty())
+		{
+			univercity = newUniver;
+		}
+		else
+		{
+			cout << "Univercity Error!" << endl;
+		}
+	}
+	void setCityUniver(const string& newCityUniver)
+	{
+		if (!newCityUniver.empty())
+		{
+			cityUnivercity = newCityUniver;
+		}
+		else
+		{
+			cout << "CityUnivercity Error!" << endl;
+		}
+	}
+	void setCountryUniver(const string& newCountryUniver)
+	{
+		if (!newCountryUniver.empty())
+		{
+			countryUnivercity = newCountryUniver;
+		}
+		else
+		{
+			cout << "CountryUnivercity Error!" << endl;
+		}
+	}
+	void setNumberGroup(const size_t& newNumberGroup)
+	{
+		if (newNumberGroup >0)
+		{
+			numberGroup = newNumberGroup;
+		}
+		else
+		{
+			cout << "NumberGroup Error!" << endl;
+		}
+	}
+
+private:
+	string name;
+	string surname;
+	string fathername;
+	Date date;
+	string number;
+	string city;
+	string country;
+	string univercity;
+	string cityUnivercity;
+	string countryUnivercity;
+	string numberGroup;
+	bool checkDigit(string check) {
+		for (int i = 0; i < size(check); i++)
+		{
+			if (isdigit(check[i]))
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+};
+
+int main() {
+	string str;
+
+	return 0;
+}
